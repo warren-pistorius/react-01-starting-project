fb1183de924eeabd794f3e9ab85384c42b996f4c
Warren Pistorius <warren@sleevesup.com.au>
2025-02-21 08:23:48 +0800

    Initial setup for coreconcepts

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          {/* Using the spread operator here to simplify passing in each property. 
+            See: https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/39649038#overview */}
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,10 @@
+//WITH Destructuring of props
+export default function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
